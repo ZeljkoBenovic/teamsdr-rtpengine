6922b06e7b42b06a3258d8422f04885fe3478ec5
ZeljkoBenovic <47507986+ZeljkoBenovic@users.noreply.github.com>
2020-05-28 14:09:02 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,7 @@ sed -i -e "s/MY_NG_IP/$LOCAL_IP/g" /etc/rtpengine.conf
 sed -i -e "s/MY_IP/$MY_IP/g" /etc/rtpengine.conf
 sed -i -e "s/RTP_MIN/$RTP_MIN/g" /etc/rtpengine.conf
 sed -i -e "s/RTP_MAX/$RTP_MAX/g" /etc/rtpengine.conf
+sed -i -e "s/NG_LISTEN/$NG_LISTEN/g" /etc/rtpengine.conf
 
 if [ "$1" = 'rtpengine' ]; then
   shift
